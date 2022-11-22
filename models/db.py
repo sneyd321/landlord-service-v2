2473efca2915208522aa3f2100e4c0c009d79caf
Ryan Sneyd <rts1234567@hotmail.com>
2022-11-22 11:53:02 -0500

    attempt to fix pending transaction error

--- models/db.py
+++ models/db.py
@@ -11,8 +11,13 @@ class DB:
 
     def __init__(self, user, password, host, database):
         self.engine = create_async_engine(f"mysql+aiomysql://{user}:{password}@{host}/{database}", pool_pre_ping=True)
+        
         Session = sessionmaker(bind=self.engine, expire_on_commit=False, class_=AsyncSession)
         self.session = Session()
+        try:
+            _ = self.session.connection()
+        except PendingRollbackError:
+            self.session.rollback()
         
     def get_session(self):
         return self.session
